2a86beaf1891c8f76e49d1b7c9a3f2efeee4f468
Goodness Igunma <Goodnessigunma1@gmail.com>
2022-05-10 16:58:45 +0100

    Added Font-awesome

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,5 +1,40 @@
 <template>
-  <div>
+  <div class="container">
+    <h1 class="text-center" mt-5>Vue Todo List</h1>
+
+    <!-- Input -->
+    <div class="d-flex">
+      <input type="text" class="form-control">
+      <button class="btn btn-warning rounded-0">Submit</button>
+    </div>
+    <br>
+
+    <!-- Task Table -->
+    <table class="table table-hover table-dark">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col">Status</th>
+          <th scope="col">#</th>
+          <th scope="col">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Buy snacks for this evening</td>
+          <td>pending</td>
+          <td>@games</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Play Uncharted this night</td>
+          <td>todo</td>
+          <td>@fat</td>
+        </tr>
+      </tbody>
+    </table>
+
 
   </div>
 </template>
